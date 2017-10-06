7e1a6813aab990ea462813d33ae6f766174c5470
lc9900 <whynot_hm@hotmail.com>
2017-10-06 19:09:46 -0400

    add item to cart

--- server/api/lineitems.js
+++ server/api/lineitems.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const LineItem = require('../db/LineItem');
 module.exports = router;
 
-router.get('/', (req, res) => {
+router.post('/', (req, res, next) => {
+    const {orderId, lineItemObj} = req.body;
+    LineItem.addItemToCart(orderId*1, lineItemObj)
+            .catch(err => {
+                next(err);
+            })
 });
 
 router.delete('/', (req, res) => {

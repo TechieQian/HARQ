65146967fc779165c74ab1efb471c49ca0d71dee
lc9900 <whynot_hm@hotmail.com>
2017-10-06 18:47:53 -0400

    removed test res.send

--- server/api/lineitems.js
+++ server/api/lineitems.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 module.exports = router;
 
 router.get('/', (req, res) => {
-    res.send('You got /api/lineitems');
 });
 
 router.delete('/', (req, res) => {
-    res.send('You delete /api/lineitems');
 });

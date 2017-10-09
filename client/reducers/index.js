7be54a45a9c63eb82580097b776d1f775dc22dd2
lc9900 <whynot_hm@hotmail.com>
2017-10-08 20:00:15 -0400

    created user reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,13 +2,16 @@ import {combineReducers} from 'redux'
 import cart from './cart'
 import products from './products'
 import users from './users'
+import user from './user'
 
 export default combineReducers({
 	cart,
 	products,
-	users
+	users,
+    user
 })
 
 export * from './cart'
 export * from './products'
 export * from './users'
+export * from './user'
---
+++ client/reducers/user.js
@@ -0,0 +1,39 @@
+// User reducer
+import axios from 'axios';
+
+// ACTION TYPES
+const SET_CURRENT_USER = 'SET_CURRENT_USER';
+
+
+// ACTION CREATORS
+export function setCurrentUser(user) {
+    return {
+        type: SET_CURRENT_USER,
+        user
+    };
+}
+
+// THUNK
+export function verifyUser(credential){
+    return axios.post('/api/user/auth', credential)
+            .then(res => res.data)
+            .then(user => {
+                if (user) {
+                    dispatch(setCurrentUser(user));
+                }
+            })
+            .catch(err => { throw err; });
+}
+
+
+// REDUCER
+export default function reducer (state = {}, action) {
+    switch (action.type) {
+
+        case SET_CURRENT_USER:
+            return action.user;
+
+        default:
+            return state;
+    }
+}
